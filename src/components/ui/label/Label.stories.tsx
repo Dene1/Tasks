c24b20829e4bdba1876b172bd1cc77d8bdcdc1cf
Denel <churakov1998@mail.ru>
2026-05-19 21:03:42 +0300

    test: добавил интерактивный тест для Label.stories

--- src/components/ui/label/Label.stories.tsx
+++ src/components/ui/label/Label.stories.tsx
@@ -1,11 +1,12 @@
 import type {Meta, StoryObj} from '@storybook/react-vite'
 import {Label} from './Label'
+import {userEvent, within, expect, fn} from 'storybook/test'
+import {useArgs} from 'storybook/preview-api'
 
 const meta: Meta<typeof Label> = {
 	component: Label,
 	tags: ['autodocs'],
 	args: {
-		toggleTodoSelection: (id: string) => console.log('Toggle selection:', id),
 		item: {
 			id: '1',
 			title: 'Example Todo Item',
@@ -19,6 +20,7 @@ export default meta
 
 type Story = StoryObj<typeof Label>
 
+
 export const Default: Story = {
 	args: {
 		item: {
@@ -27,6 +29,31 @@ export const Default: Story = {
 			isSelected: false,
 			isCompleted: false
 		}
+	},
+	render: function Render(args) {
+		const [{item}, updateArgs] = useArgs()
+
+		const toggleTodoSelection = () => {
+			updateArgs({
+				item: {
+					...item,
+					isSelected: !item.isSelected
+				}
+			})
+		}
+
+		return (
+			<Label  {...args} item={item}
+			        toggleTodoSelection={toggleTodoSelection} />
+		)
+	},
+	play: async ({canvasElement}) => {
+		const canvas = within(canvasElement)
+		const checkbox = canvas.getByRole('checkbox')
+
+		await userEvent.click(checkbox)
+		// expect(checkbox).toBeChecked()
+		expect(checkbox).not.toBeChecked()
 	}
 }
 
